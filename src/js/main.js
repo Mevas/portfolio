68c26b2b580b5b6deef2096147340ebc64f46312
Alexandru Vasilescu <alexandru.star@gmail.com>
2018-01-29 03:31:01 +0200

    Added string localization

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const statements = [{
-  input: 'Alex.currentLocation',
+  input: 'Alex.current_location',
   return: '"Bucharest, Romania"',
 },
 {
@@ -17,8 +17,10 @@ const statements = [{
 ];
 // https://pbs.twimg.com/profile_images/802981289771016193/7F3ZaKNQ.jpg - place-holder
 const projects = [{
-  title: 'Portfolio',
-  description: 'This page! Responsive website made with nothing other than pure HTML5, CSS3 and JavaScript, auto-generates content for easy maintenance.',
+  title_en: 'Portfolio',
+  title_ro: 'CV',
+  description_en: 'This page! Responsive website made with nothing other than pure HTML5, CSS3 and JavaScript, auto-generates content for easy maintenance.',
+  description_ro: 'Pagina aceasta! Website responsive creat cu HTML5, CSS3 și JavaScript pur, generează conținutul automat pentru o ușoară mentenanță.',
   skills: ['HTML5', 'CSS3', 'JavaScript'],
   screenshot: 'src/images/portfolio.png',
   links: {
@@ -52,11 +54,11 @@ function typeWriter(elementId, string) {
   element.innerHTML += caret;
 }
 
-typeWriter('main-title', 'Welcome');
-
 function createProjectCard({
-  title,
-  description,
+  title_en,
+  title_ro,
+  description_en,
+  description_ro,
   skills,
   screenshot,
   links,
@@ -66,20 +68,22 @@ function createProjectCard({
       <div class="project-image" style="background: url(${screenshot}) 0% 0% / cover no-repeat;"></div>
       <div class="project-content">
         <div class="project-info">
-          <h1 class="project-title">${title}</h1>
-          <p class="project-description">${description}</p>
-          <div class="project-skills project-skills-${title}">
+          <h1 class="project-title localize" lang="en">${title_en}</h1>
+          <h1 class="project-title localize" lang="ro">${title_ro}</h1>
+          <p class="project-description localize" lang="en">${description_en}</p>
+          <p class="project-description localize" lang="ro">${description_ro}</p>
+          <div id="project-skills-${title_en}" class="project-skills">
 
           </div>
         </div>
         <div class="project-links">
-          <a href="${links.github}"><span>View Code</span></a>
-          <a href="${links.preview}"><span>Explore Demo</span></a>
+          <a href="${links.github}"><span class="localize" lang="en">View Code</span><span class="localize" lang="ro">Codul </span></a>
+          <a href="${links.preview}"><span class="localize" lang="en">Explore Demo</span><span class="localize" lang="ro">Demo</span></a>
         </div>
       </div>
     </div>`;
   skills.forEach((skill) => {
-    document.getElementsByClassName(`project-skills-${title}`)[0].innerHTML += `<span class="project-skill">${skill}</span>`;
+    document.getElementById(`project-skills-${title_en}`).innerHTML += `<span class="project-skill">${skill}</span>`;
   });
 }
 
@@ -111,6 +115,26 @@ function createContact(name, url) {
 contactInfo.forEach((contact) => {
   createContact(contact.name, contact.url);
 });
+
+function selectLanguage(language) {
+  i = 0;
+  document.getElementById('main-title').innerHTML = '> ';
+  if (language === 'en') {
+    typeWriter('main-title', 'Welcome');
+  } else {
+    typeWriter('main-title', 'Bun venit');
+  }
+
+  Array.from(document.getElementsByClassName('localize')).forEach((element) => {
+    if (element.lang === language) {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+
+selectLanguage('en');
 
 function getPos(el) {
   const rect = el.getBoundingClientRect();
